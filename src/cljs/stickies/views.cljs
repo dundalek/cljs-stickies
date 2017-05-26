34866dc44528df51a262ed823e7d9b9a1533e4f2
Jakub Dundalek <dundalek@gmail.com>
2017-05-26 12:46:35 +0200

    Initial sticky notes

--- src/cljs/stickies/views.cljs
+++ src/cljs/stickies/views.cljs
@@ -1,7 +1,19 @@
 (ns stickies.views
     (:require [re-frame.core :as re-frame]))
 
+(def notes
+  [{:id 1 :name "aa" :content "abc" :color "#ffc"}
+   {:id 2 :name "bb" :content "abc" :color "#cfc"}
+   {:id 3 :name "cc" :content "abc" :color "#ccf"}
+   {:id 4 :name "dd" :content "abc" :color "#fcc"}
+   {:id 5 :name "ee" :content "abc" :color "#ffc"}])
+
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div "Hello from " @name])))
+      [:ul
+        (for [{:keys [id name content color]} notes]
+          ^{:key id} [:li
+                        [:a {:href "#" :style {:background color}}
+                          [:h2 name]
+                          [:p content]]])])))
