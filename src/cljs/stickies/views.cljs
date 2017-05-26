70babe7d6e31fed8ad143af3ee9dfc5e0bd699e5
Jakub Dundalek <dundalek@gmail.com>
2017-05-26 12:58:17 +0200

    Better markup

--- src/cljs/stickies/views.cljs
+++ src/cljs/stickies/views.cljs
@@ -2,18 +2,20 @@
     (:require [re-frame.core :as re-frame]))
 
 (def notes
-  [{:id 1 :name "aa" :content "abc" :color "#ffc"}
-   {:id 2 :name "bb" :content "abc" :color "#cfc"}
-   {:id 3 :name "cc" :content "abc" :color "#ccf"}
-   {:id 4 :name "dd" :content "abc" :color "#fcc"}
-   {:id 5 :name "ee" :content "abc" :color "#ffc"}])
+  [{:id 1 :name "aa" :content "abc" :color "#ffc" :rotate "-6deg"}
+   {:id 2 :name "bb" :content "abc" :color "#cfc" :rotate "4deg"}
+   {:id 3 :name "cc" :content "abc" :color "#ccf" :rotate "3deg"}
+   {:id 4 :name "dd" :content "abc" :color "#fcc" :rotate "-5deg"}
+   {:id 5 :name "ee" :content "abc" :color "#ffc" :rotate "5deg"}])
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:ul
-        (for [{:keys [id name content color]} notes]
-          ^{:key id} [:li
-                        [:a {:href "#" :style {:background color}}
-                          [:h2 name]
-                          [:p content]]])])))
+      [:div
+        (for [{:keys [id name content color rotate]} notes]
+          ^{:key id} [:div.sticky
+                        {:style
+                          {:background color
+                           :transform (str "rotate(" rotate ")")}}
+                        [:div.sticky-header name]
+                        [:div.sticky-content content]])])))
