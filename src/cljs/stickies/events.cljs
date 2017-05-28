23459e22d804cc3e74f548bfc983833bba952158
Jakub Dundalek <dundalek@gmail.com>
2017-05-28 18:34:31 +0200

    Save changes to backend

--- src/cljs/stickies/events.cljs
+++ src/cljs/stickies/events.cljs
@@ -1,6 +1,27 @@
 (ns stickies.events
     (:require [re-frame.core :as re-frame]
+              [ajax.core :refer [PUT]]
               [stickies.db :as db]))
+
+(defn handler [response]
+  (.log js/console (str response)))
+
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "something bad happened: " status " " status-text)))
+
+(defn update-item [id data]
+   (PUT (str "http://localhost:3030/item/" id)
+      {:format :json
+       :params data
+       :response-format :json
+       :keywords? true
+       :handler handler
+       :error-handler error-handler}))
+
+(def update-item-interceptor
+   (re-frame/after
+      (fn [db [_ id data]]
+         (update-item id (dissoc ((:notes db) id) :rotate)))))
 
 (re-frame/reg-event-db
  :initialize-db
@@ -9,6 +30,7 @@
 
 (re-frame/reg-event-db
  :update-note
+ [update-item-interceptor]
  (fn  [db [_ id data]]
    (let [next-db (update-in db [:notes id] #(merge % data))]
       ; (js/console.log (prn-str (vals (:notes next-db))))
